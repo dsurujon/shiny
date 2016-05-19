48f420b17343204629c96dfebef286d85616a04d
Defne <defnesurujon@gmail.com>
2016-05-19 10:08:05 -0400

    Update server.R
    added an option for 2-way anova

--- MRI/server.R
+++ MRI/server.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 #some global variables
-values<-reactiveValues(data_submitted=FALSE,labels_submitted=FALSE,pvals=NULL,mriraw1=NULL,mrilables=NULL)
+values<-reactiveValues(data_submitted=FALSE,labels_submitted=FALSE,pvals=NULL,mriraw1=NULL,mrilables=NULL,twowayANOVA=FALSE)
 
 shinyServer(function(input, output) {	
 
@@ -24,9 +24,30 @@ shinyServer(function(input, output) {
 			print("Labels file submitted")
 			values$labels_submitted=TRUE
 			values$mrilabels<-read.delim(input$MRI_labels$datapath,header=T, sep="\t")
+			totalnumcats<-ncol(values$mrilabels)-1
+			values$twowayANOVA<-(totalnumcats>1)
 		})
 	})
 
+	##Dropdown list generated from the label categories
+	output$select_category1<-renderUI({
+		if(!is.null(values$mrilabels)){
+			numcats<-ncol(values$mrilabels)-1
+			select.choices<-setNames(names(values$mrilabels)[1:numcats+1],names(values$mrilabels)[1:numcats+1])
+			selectInput("select_cat1", label = "Select category 1", choices = select.choices,select.choices[1])
+			
+		}
+	})
+	output$select_category2<-renderUI({
+		if(values$twowayANOVA){
+			numcats<-ncol(values$mrilabels)-1
+
+			#select.choices<- list(names(values$mrilabels)[2:numcats+1]
+			select.choices<-setNames(names(values$mrilabels)[1:numcats+1],names(values$mrilabels)[1:numcats+1])
+			selectInput("select_cat2", label = "Select category 2", choices = select.choices,selected=select.choices[2])
+
+		}
+	})
 	##Generate the significance summary of the data
 	output$MRIdata <- renderTable({
 		#Don't compute anything unless both files have been submitted
@@ -38,8 +59,9 @@ shinyServer(function(input, output) {
 		averaged <- aggregate(mriraw2[c("Fat", "Lean", "Weight")], list(id=mriraw2$Label), mean)
 		mri <- within(averaged, {Fbw <- Fat/Weight*100})
 		mat2 <- merge(mri, values$mrilabels, by="id")
-		mat2 <- mat2[c("id", "gp", "Fat", "Lean", "Weight", "Fbw")]
-		groups<-list(gp=mat2$gp)
+		if(values$twowayANOVA){mat2$group000<-paste0(as.character(mat2[input$select_cat1][,1]),".",as.character(mat2[input$select_cat2][,1]))}
+		else{mat2$group000<-as.character(mat2[input$select_cat1][,1])}
+		groups<-list(group000=mat2$group000)
 		mri.values <- aggregate(mat2[c("Fat", "Lean", "Weight", "Fbw")],groups,mean, simplify=TRUE)
 		rownames(mri.values)<-mri.values[,1]
 		mri.values <- mri.values[,2:ncol(mri.values)]
@@ -48,23 +70,39 @@ shinyServer(function(input, output) {
 		
 		#compute SEM
 		stderr <- function(x) sqrt(var(x)/length(x))
-		mri.errbar <- aggregate(mat2[c("Fat", "Lean", "Weight", "Fbw")],list(gp=mat2$gp),stderr, simplify=TRUE)
+		mri.errbar <- aggregate(mat2[c("Fat", "Lean", "Weight", "Fbw")],list(group000=mat2$group000),stderr, simplify=TRUE)
 		rownames(mri.errbar) <- mri.errbar[,1]
 		mri.errbar[,1] <- NULL
 		mri.errbar <- as.matrix(mri.errbar)
 		values$mri.errbar<-mri.errbar
 		
-		#compute t-test p values through anova (in case there are >2 cohorts) and then Tukey's post-hoc test
-		pvalues<-function(x){
-			tuk<-TukeyHSD(aov(mat2[x][,1]~mat2["gp"][,1]))
-			return (tuk[[1]][,4])
-		} 
-		
-		#clean and display the p-values
-		pvals<-sapply(c("Fat","Lean","Weight","Fbw"),pvalues)
-		pval_results<-as.data.frame(pvals)
-		pval_results
-		
+		if(values$twowayANOVA){
+			print("computing anova")
+
+			multi.anova<-function(x){
+				mat2.aov<-aov(mat2[x][,1]~mat2[input$select_cat1][,1]*mat2[input$select_cat2][,1])
+				summary.mat2.aov<-as.data.frame(summary(mat2.aov)[[1]])
+				rownames(summary.mat2.aov)<-c(paste(x,input$select_cat1,sep=" "),paste(x,input$select_cat2,sep=" "),paste(x,input$select_cat1,":",input$select_cat2,sep=" "),paste(x,"Residuals",sep=" "))
+				return(summary.mat2.aov)
+			}
+			pvals<-data.frame()
+			for (i in c("Fat","Lean","Weight","Fbw")){pvals<-rbind(pvals,multi.anova(i))}
+			pvals
+		}
+		else{
+			#compute t-test p values through anova (in case there are >2 cohorts) and then Tukey's post-hoc test
+			print("computing p-values")
+			pvalues<-function(x){
+				tuk<-TukeyHSD(aov(mat2[x][,1]~mat2["group000"][,1]))
+				print(tuk)
+				return (tuk[[1]][,4])
+			} 
+			
+			#clean and display the p-values
+			pvals<-sapply(c("Fat","Lean","Weight","Fbw"),pvalues)
+			pval_results<-as.data.frame(pvals)
+			pval_results
+		}
 	})
 
 	##Generate a bar plot with standard error. Groups are stacked.
