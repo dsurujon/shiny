5e488332b7a346d0ee4d4cd470cece6c04d12849
Defne <defnesurujon@gmail.com>
2017-03-05 17:30:52 -0500

    add strain selection upload/download, png download

--- pangenome_app/server.r
+++ pangenome_app/server.r
@@ -5,20 +5,22 @@ source("customdistance.r")
 
 presence_table<-read.csv("data/374_clusters.csv",header=T,stringsAsFactors = FALSE)
 presence_table[presence_table==" "]<-NA
-#product<-presence_table$Product
-#presence_table$Product<-NULL
-#presence_table<-presence_table[ , order(namrunes(presence_table))]
-#presence_table<-cbind(product,presence_table)
+
 values<-reactiveValues()
 xv<-ncol(presence_table)
 allstrains<-names(presence_table)[2:xv]
+
+refstrains<-c("ATCC","AP200","BHN97",'R6','SPNA45','ST556',
+              'TCH84331.19A','TIGR4','X19A','X19F','X670.6B',
+              'X70585','CGSP14','D39','G54','gamPNI0373',
+              'INV104','INV200','JJA','OXC141','P1031')
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output,session) {
   output$strains<-renderPrint(allstrains)
   
   
-  
+  #get the gene x-reference table for the selected strains
   prepare_subtable<-reactive({
     st<-cbind(presence_table[,c("Product",input$mychooser$right)])
     xc<-ncol(st)
@@ -38,6 +40,27 @@ shinyServer(function(input, output,session) {
     
   })
   
+  #get uploaded strain selection
+  observe({
+    infile<- input$strainslist
+    if (is.null(infile)){
+      values$leftselection<-allstrains
+      values$rightselection<-c()
+      return(NULL)}
+    else{
+      strainslist<-input$strainslist
+      strains<-readLines(strainslist$datapath)
+      values$leftselection<-setdiff(allstrains,strains)
+      values$rightselection<-strains
+    }
+  })
+  
+  output$make_selector<-renderUI(
+    chooserInput("mychooser", "Available organisms", "Selected organisms",
+                 values$leftselection, values$rightselection, size = 10, multiple = TRUE)
+  )
+  
+  #get the table of unique genes for each strain for the selected strains
   prepare_uniques<-reactive({
     acctable<-prepare_subtable()$accessorytable
     numberNAs<-ncol(acctable)-2 #removing product, and one gene
@@ -47,6 +70,7 @@ shinyServer(function(input, output,session) {
     return(list("selectionnames"=selectionnames,"fulluniquetable"=acctable))
   })
   
+  #get the unique genes for a single selected strain
   prepare_singleunique<-reactive({
     acctable<-prepare_uniques()$fulluniquetable
     unique_genes<-acctable[,c("Product",input$uniquestrain)]
@@ -54,25 +78,32 @@ shinyServer(function(input, output,session) {
     return(list("singleuniquetable"=unique_genes))
     
   })
+  
+  #selection of strains
   output$selectionnames<-renderText(input$mychooser$right)
   
+  #output the full xref table
   output$selection <- renderDataTable(
     tryCatch(
       prepare_subtable()$subtable
       , error=function(e) message("No strains are selected."))
   )
   
+  #output the core xref table
   output$selectionCore <- renderDataTable(
     tryCatch(
       prepare_subtable()$coretable
       , error=function(e) message("No strains are selected."))
   )
+  
+  #output the accessory xref table
   output$selectionAcc <- renderDataTable(
     tryCatch(
       prepare_subtable()$accessorytable
       , error=function(e) message("No strains are selected."))
   )
   
+  #output the size of the core and pan genomes for the selection of strains
   output$coreandpan<-renderText(
     tryCatch(
       paste("Pangenome size:",as.character(prepare_subtable()$pansize),
@@ -80,7 +111,8 @@ shinyServer(function(input, output,session) {
       , error=function(e) return("No strains selected"))
   )
   
-  output$distance_phylogeny<-renderPlot(
+  #plot the dendrogram of selected strains based on gene distance
+  make_dendrogram<-function(){
     tryCatch(
       plot(hclust(as.dist(prepare_subtable()$distances)),xlab="",sub="")
       , error=function(e) {
@@ -91,8 +123,14 @@ shinyServer(function(input, output,session) {
         img[transparent]<-NA
         plot(1:2,type='n',xaxt='n', ann=FALSE, yaxt='n',bty='n')
         rasterImage(img, 1.1, 1.1, 1.7, 1.8, interpolate=FALSE)})
+  }
+  
+  #display dendro as plot 
+  output$distance_phylogeny<-renderPlot(
+    make_dendrogram()
   )
   
+  #download the full xref table
   output$subtable_dl<-downloadHandler(
     filename=function(){
       paste0(Sys.Date(),".genetable.csv")
@@ -101,6 +139,7 @@ shinyServer(function(input, output,session) {
       write.csv(prepare_subtable()$subtable,file,row.names=FALSE)	
     })
   
+  #download the core xref table
   output$subtableCore_dl<-downloadHandler(
     filename=function(){
       paste0(Sys.Date(),".core.genetable.csv")
@@ -109,6 +148,7 @@ shinyServer(function(input, output,session) {
       write.csv(prepare_subtable()$coretable,file,row.names=FALSE)	
     })
   
+  #download the accessory xref table
   output$subtableAcc_dl<-downloadHandler(
     filename=function(){
       paste0(Sys.Date(),".accessory.genetable.csv")
@@ -117,14 +157,18 @@ shinyServer(function(input, output,session) {
       write.csv(prepare_subtable()$accessorytable,file,row.names=FALSE)	
     })
   
+  #strain selector (dropdown UI) for the unique genes
   output$unique_selector<-renderUI(
     selectInput("uniquestrain","Make a selection to get genes unique to that strain",
                 choices=prepare_uniques()$selectionnames)
   )
+  
+  #output the unique genes table
   output$unique_for_strain<-renderDataTable(
       prepare_singleunique()$singleuniquetable
   )
   
+  #download the unique genes table
   output$subtableUniqu_dl<-downloadHandler(
     filename=function(){
       paste0(Sys.Date(),".unique.genetable.csv")
@@ -132,5 +176,29 @@ shinyServer(function(input, output,session) {
     content=function(file){
       write.csv(prepare_singleunique()$singleuniquetable,file,row.names=FALSE)	
     })
+  
+  #download the dendro as png image
+  output$dendrogram_dl<-downloadHandler(
+    filename=function(){
+      paste0(Sys.Date(),".dendrogram.png")
+    },
+    content=function(file){
+      png(file)
+      make_dendrogram()
+      dev.off()
+    }
+  )
+  
+  #download list of selected strains
+  output$current_selection<-downloadHandler(
+    filename=function(){
+      paste0(Sys.Date(),".strains.txt")
+    },
+    content=function(file){
+      write(prepare_uniques()$selectionnames,file)
+    }
+  )
+  
+  #upload txt file with strain selection
   
 })
--- pangenome_app/ui.r
+++ pangenome_app/ui.r
@@ -19,34 +19,37 @@ shinyUI(fluidPage(
 	sidebarPanel(
 		tags$small(paste0("Select the strains you want to examine from the left. ",
 		"The selected strains will appear on the right-hand box. ")),
-		chooserInput("mychooser", "Available organisms", "Selected organisms",
-			allstrains, c(), size = 10, multiple = TRUE),
+		uiOutput("make_selector"),
+		fileInput('strainslist',"Upload strain selection"),
+		downloadButton('current_selection',"Download selected strains list"),
 		verbatimTextOutput("coreandpan")
-	),
+	), #/sidebarPanel
 	
-	mainPanel(plotOutput("distance_phylogeny") )
-	),
+	mainPanel(plotOutput("distance_phylogeny"),
+	          downloadButton('dendrogram_dl',"Download dendrogram")
+	          )#/mainpanel
+	), #/fluidRow
 	tabsetPanel(
 	  tabPanel("Full Gene Table",
 	 
 		tags$h2("Gene table for the selected strains"),
 		downloadButton('subtable_dl',"Download gene table"),
-		dataTableOutput("selection")),
+		dataTableOutput("selection")), #tabpanel1
 
 	  tabPanel("Core Gene Table",
 	           tags$h2("Gene table for the core genome of the selected strains"),
 	           downloadButton('subtableCore_dl',"Download gene table"),
-	           dataTableOutput("selectionCore")),
+	           dataTableOutput("selectionCore")), #tabpanel2
 
 	  tabPanel("Accessory Gene Table",
 	           tags$h2("Gene table for the accessory genome of the selected strains"),
 	           downloadButton('subtableAcc_dl',"Download gene table"),
-	           dataTableOutput("selectionAcc")),
+	           dataTableOutput("selectionAcc")), #/tabpanel3
 		
 		tabPanel("Unique Genes",
 		         #downloadButton('subtableAcc_dl',"Download gene table"),
 		         uiOutput("unique_selector"),
 		         downloadButton('subtableUniqu_dl',"Download gene table"),
-		         dataTableOutput("unique_for_strain"))
-	)
-))
+		         dataTableOutput("unique_for_strain")) #/tabpanel4
+	) #/tabsetpanel
+))  #/fluidPage /shinyUI
